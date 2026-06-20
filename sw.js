7e80aedb38c3fb907a61832f7ba742074b626000
Mtc22702 <matcar22@yahoo.com>
2026-06-20 23:12:45 +0300

    bugfix scroll scheda pianta

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Unico punto di versionamento dell'app. Cambialo quando pubblichi una release.
-const CACHE_VERSION = "2026-06-20-135";
+const CACHE_VERSION = "2026-06-20-137";
 const CACHE = `serra-${CACHE_VERSION}`;
 
 const PRECACHE = [
