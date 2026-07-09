dbd77a457b27f4a507e305a7902c339903b9574b
Mtc22702 <camat2022@hotmail.com>
2026-07-09 10:02:44 +0300

    aumento numero versione

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Configurazione e risorse in cache */
-const CACHE_VERSION = "2026-07-08-perf15";
+const CACHE_VERSION = "2026-07-09-ui1";
 const CACHE = `serra-${CACHE_VERSION}`;
 
 const PRECACHE = [
