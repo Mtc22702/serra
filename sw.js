cecf1094a75c651bc97fae684b1e69d79a0fbff5
Mtc22702 <matcar22@yahoo.com>
2026-07-13 10:40:25 +0300

    bugfix allineamento testi dentro pulsanti

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Configurazione e risorse in cache */
-const CACHE_VERSION = "2026-07-13-guided-accordion-ui";
+const CACHE_VERSION = "2026-07-13-button-align-global";
 const CACHE = `serra-${CACHE_VERSION}`;
 
 const PRECACHE = [
