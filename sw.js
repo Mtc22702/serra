b4bb775ffe7b578261d5510ce39c1ab3ed102a84
Matteo <camat2022@hotmail.com>
2026-07-15 19:04:23 +0300

    miglioramento grafica sfondi e ui/ux pagina index

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Definisce versione e risorse statiche da memorizzare nella cache dell'applicazione. */
-const CACHE_VERSION = "2026-07-15-heated-modal";
+const CACHE_VERSION = "2026-07-15-hero-vertical-rhythm";
 const CACHE = `serra-${CACHE_VERSION}`;
 
 const PRECACHE = [
