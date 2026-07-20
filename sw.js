a6b1009eaa48ad6fb1733bc542feec921b9cf8e2
Matteo <camat2022@hotmail.com>
2026-07-20 15:18:43 +0300

    pagina index veocizzare crescita pinate

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Definisce versione e risorse statiche da memorizzare nella cache dell'applicazione. */
-const CACHE_VERSION = "20260720-1bf45bd530bd";
+const CACHE_VERSION = "20260720-9edd1c4d0dfe";
 // Incrementato a ogni rilascio forzato: assicura una nuova impronta della
 // cache anche quando la modifica è principalmente grafica o di distribuzione.
 const RELEASE_EPOCH = 2;
